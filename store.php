96f41cec963d0cff63d3f512ef22a50efcf03f82
sikakamura <you@example.com>
2024-12-27 00:03:09 +0700

    1

--- store.php
+++ store.php
@@ -1,7 +1,9 @@
 <?php 
 
 $myfile = fopen("location.txt", "w");
-$txt = "lat: " . $_GET["lat"] . "long:" . "\nlong" . $_GET["long"];
+$txt = "lat: " . $_GET["lat"]  . "\nlong: " . $_GET["long"];
 fwrite($myfile, $txt);
 fclose($myfile);
+
+
 ?>
